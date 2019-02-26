17cd67335f79252d334a9975c5b5984687e26d5d
Katerina Loschinina <ekaterina.loshchinina@gmail.com>
2019-02-26 00:25:50 +0000

    Two arduinos sends and receives through pi
    Sensors send information to pi and LEDs receive information from pi
    
    Two way communication!

--- pi-brain/server.js
+++ pi-brain/server.js
@@ -1,58 +1,57 @@
-
 const SerialPort = require('serialport')
 const Readline = require('@serialport/parser-readline')
-const port = new SerialPort('/dev/tty.usbmodem14101')
-const parser = port.pipe(new Readline({ delimiter: '\n' }))
-
-var lastSendTime = 0
-
-
-
-// Read data that is available but keep the stream in "paused mode"
-port.on('readable', function () {
-  console.log('Data:', port.read())
-  port.write("1\n", function(err) {
-    if (err) {
-      return console.log('Error on write: ', err.message)
-    }
-  })
+const port1 = new SerialPort('/dev/ttyACM1', {
+  baudRate: 9600
 })
+const parser1 = port1.pipe(new Readline({ delimiter: '\n' }))
 
-parser.on('data', function (data) {
-  dataStr = data.toString('utf8');
-  console.log('Data:', dataStr)
-
-  const sensorValue = parseInt(dataStr, 10)
-  console.log('Sensor value:', sensorValue)
-
-  // do the math
-  const outVale = Math.floor((sensorValue / 200.0) * 40) //number of LEDs to light up
-
-  lastSendTime = Date.now()
-  port.write(outVale + "\n", function(err) {
-    console.log("Send value:", outVale)
-    if (err) {
-      return console.log('Error on write: ', err.message)
-    }
-  })
+const port2 = new SerialPort('/dev/ttyACM2', {
+  baudRate: 9600
 })
+const parser2 = port2.pipe(new Readline({ delimiter: '\n' }))
 
-// Open errors will be emitted as an error event
-port.on('error', function(err) {
-  console.log('Error: ', err.message)
-})
+const ports = [port1, port2]
 
+const parsers = [parser1, parser2]
 
-// Send heartbeat signal every 3 seconds
-setInterval(function() {
-  if (Date.now() - lastSendTime < 3000){
-    return
-  }
+const dividers = [200.0 , 500.0]
 
-  console.log('Heartbeat')
-  port.write(5 + "\n", function(err) {
-    if (err) {
-      return console.log('Error on write: ', err.message)
-    }
+parsers.map( (parser, key) => {
+  let lastSendTime = 0
+
+  parser.on('data', function (data) {
+    const sensorValue = parseInt(data, 10)
+    console.log(`Sensor value ${key}:`, sensorValue)
+
+    // do the math
+    const outVale = Math.floor((sensorValue / dividers[key]) * 40) //number of LEDs to l$
+
+    lastSendTime = Date.now()
+    ports[key].write(outVale + "\n", function(err) {
+      console.log(`Send value ${key}:`, outVale)
+      if (err) {
+        return console.log('Error on write: ', err.message)
+      }
+    })
   })
-}, 1000)
+
+  // Open errors will be emitted as an error event
+  ports[key].on('error', function(err) {
+    console.log('Error: ', err.message)
+  })
+
+
+  // Send heartbeat signal every 3 seconds
+  setInterval(function() {
+    if (Date.now() - lastSendTime < 3000){
+      return
+    }
+
+    console.log(`Heartbeat ${key}`)
+    ports[key].write(5 + "\n", function(err) {
+      if (err) {
+        return console.log('Error on write: ', err.message)
+      }
+    })
+  }, 1000)
+})
