3a2d7ff898ecc778c1a54f8c448d8e5a2856c304
IvanIsakov <ivan.isakov@gmail.com>
2019-02-28 12:02:56 +0000

    Arduino. Running average calculation for sensor value. Web. Flicker values.
    Revert "Revert "Update app.ino""
    
    This reverts commit d404375da9c8d9e6f374489f315d1ebb58280242.
    
    Revert "Revert "Added Flicker function""
    
    This reverts commit e40feee27a2ab2055a29b3580251ce642469ade0.

--- web/client.js
+++ web/client.js
@@ -2,6 +2,7 @@ import { sleep } from './lib/helpers/sleep.js'
 import { drawColumns } from './lib/helpers/drawColumns.js'
 
 import { basic } from './lib/functions/basic.js'
+import { flicker } from './lib/functions/flicker.js'
 
 const connectArduinosWithColumns = async ({
     arduinos,
@@ -16,8 +17,14 @@ const connectArduinosWithColumns = async ({
                 columns
             })
             break
-        case 1:
-            console.log('No function is written for this option yet')
+		case 1:
+			flicker({
+				arduinos,
+				columns
+			})
+			break
+		case 2:
+			console.log('No function is written for this option yet')
             break
         default:
             console.log('You need to pick up function you would use')
