6a4fdb42dd1c8252f2d540ece1ca4cce3a845af3
Katerina Loschinina <ekaterina.loshchinina@gmail.com>
2019-03-21 21:30:51 +0000

    Add configurations choice with circle or duet
     - dropdown choice of configuration
     - connect different configurations to visualisation functions
     - adjust styles and positions
    
    Circle configuration contiinues to be on keys 0 - 10
    Duet configuration is on "a" and "s"

--- web/lib/functions/basic.js
+++ web/lib/functions/basic.js
@@ -1,28 +1,25 @@
-export const basic = ({
-    arduinos,
-    columns
-}) => {
-    const brightColor = '#88b'
-    for (let columnName in columns) {
-        let column = columns[columnName]
-        let arduino = arduinos[columnName]
+export const basic = ({ arduinos, columns }) => {
+  const brightColor = "#88b";
+  columns.map((columnName, key) => {
+    let column = columns[key];
+    let arduino = arduinos[key];
 
-        const tension = arduino.read()
-        const numberOfLEDs = column.numberOfLEDs
+    const tension = arduino.read();
+    const numberOfLEDs = column.numberOfLEDs;
 
-        const leds = []
+    const leds = [];
 
-        for (let key = 0; key < Math.min(tension, numberOfLEDs/2); key++) {
-            leds.push({
-                number: numberOfLEDs/2 - key,
-                color: brightColor
-            })
-            leds.push({
-                number: numberOfLEDs/2 + key,
-                color: brightColor
-            })
-        }
-
-        column.colorLeds(leds)
+    for (let key = 0; key < Math.min(tension, numberOfLEDs / 2); key++) {
+      leds.push({
+        number: numberOfLEDs / 2 - key,
+        color: brightColor
+      });
+      leds.push({
+        number: numberOfLEDs / 2 + key,
+        color: brightColor
+      });
     }
-}
+
+    column.colorLeds(leds);
+  });
+};
--- web/lib/helpers/drawColumns.js
+++ web/lib/helpers/drawColumns.js
@@ -1,23 +1,24 @@
-import { Arduino } from '../classes/arduino.js'
-import { Column } from '../classes/column.js'
+import { Arduino } from "../classes/arduino.js";
+import { Column } from "../classes/column.js";
 
-const numberOfLEDs = 40
+const numberOfLEDs = 40;
 
-export const drawColumns = (columnKeys) => {
-    const columns = []
-    const arduinos = []
+export const drawColumns = columnKeys => {
+  const columns = [];
+  const arduinos = [];
+  console.log(columnKeys);
 
-    columnKeys.forEach(key => {
-        const div = document.createElement('div')
-        div.className = `column column-${key}`
-        div.id = `column-${key}`
-        document.getElementById('container').appendChild(div)
-        columns.push(new Column(key, numberOfLEDs))
-        arduinos.push(new Arduino(0, key))
-    })
+  columnKeys.forEach(key => {
+    const div = document.createElement("div");
+    div.className = `column column-${key}`;
+    div.id = `column-${key}`;
+    document.getElementById("container").appendChild(div);
+    columns.push(new Column(key, numberOfLEDs));
+    arduinos.push(new Arduino(0, key));
+  });
 
-    return {
-        columns,
-        arduinos
-    }
-}
+  return {
+    columns,
+    arduinos
+  };
+};
