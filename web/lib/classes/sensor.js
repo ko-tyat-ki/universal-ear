e0cbfd09addfff6198a704bdf9acaaf3d434c844
IvanIsakov <ivan.isakov@gmail.com>
2019-05-29 09:08:39 +0100

    Simulating physical stretch sensor

--- web/lib/classes/sensor.js
+++ web/lib/classes/sensor.js
@@ -9,9 +9,13 @@ export class Sensor {
 		this.key = sensor.key
 		this.column = sensor.column
 		this.sensorPosition = sensor.sensorPosition
-
 		this.setKeyDownEventListener()
 		this.setKeyUpEventListener()
+		this.isSlowSensor = false
+		//this.currentTime = Date.now()
+		this.startCounting
+		this.amplitude = 1
+		this.timeCoefficient = 5
 	}
 
 	setKeyDownEventListener() {
@@ -19,6 +23,10 @@ export class Sensor {
 			"keydown",
 			event => {
 				if (event.key === this.key.toString()) {
+					if (!this.isBeingPulled) {
+						this.startCounting = Date.now()
+						this.isSlowSensor = false
+					}
 					this.isBeingPulled = true
 				}
 			},
@@ -32,6 +40,7 @@ export class Sensor {
 			event => {
 				if (event.key === this.key.toString()) {
 					this.isBeingPulled = false
+					this.startCounting = Date.now()
 				}
 			},
 			false
@@ -58,4 +67,49 @@ export class Sensor {
 
 		this.tension = Math.max(this.minimalTension, tension)
 	}
+
+	//setCurrentTime(currentTime) {
+	//	this.currentTime = currentTime
+	//}
+
+	realisticSensorUpdate() {
+		const timePassed = Date.now() - this.startCounting
+		const timeThreshold = 500 // in 10s milliseconds, i.e. 50 means 500 ms
+		if (this.key == "a") {
+			if (this.isBeingPulled) {
+				console.log("pulled", timePassed)
+				if (timePassed > timeThreshold) {
+					this.isSlowSensor = true
+					this.tension = this.slowUpTensionFormula(timePassed - timeThreshold)
+				} else {
+					this.isSlowSensor = false
+				}
+			} else {
+				console.log("not pulled", timePassed)
+				if (this.isSlowSensor) {
+					this.tension = this.slowDownTensionFormula(timePassed)
+				} else {
+					this.tension = this.fastTensionFormula(timePassed)
+				}
+			}
+		}
+	}
+
+	slowUpTensionFormula(timeValue) {
+		console.log("SLOW UP")
+		return this.amplitude * (1 - Math.exp(timeValue * this.timeCoefficient))
+	}
+
+	slowDownTensionFormula(timeValue) {
+		console.log("SLOW DOWN")
+		return this.amplitude * Math.exp(timeValue * this.timeCoefficient)
+	}
+
+	fastTensionFormula(timeValue) {
+		console.log("FAST")
+		return this.amplitude * timeValue * (1 - Math.exp(timeValue * this.timeCoefficient))
+	}
+
+
+
 }
