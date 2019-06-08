8bc6e1cf44d2dd62c193e494403da38f8954efcb
Katerina Loschinina <ekaterina.loshchinina@gmail.com>
2019-06-08 18:02:06 +0100

    Clean from unfinished mode

--- web/lib/visualisations.js
+++ web/lib/visualisations.js
@@ -2,12 +2,10 @@ import basic from './modes/basic'
 import flicker from './modes/flicker'
 import randomEcho from './modes/randomEcho'
 import tensionAndRandomEcho from './modes/tensionAndRandomEcho'
-import rainEffect from './modes/rainEffect'
 
 export default {
 	basic,
 	flicker,
 	randomEcho,
-	tensionAndRandomEcho,
-	rainEffect
+	tensionAndRandomEcho
 }
