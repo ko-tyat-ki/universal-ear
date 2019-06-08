0ea6df694f1df83224156c905e05317fdd8f9862
Oleg Gergel <gergel.oleg@yahoo.com>
2019-06-08 17:48:40 +0100

    Add basic_test mode

--- web/lib/visualisations.js
+++ web/lib/visualisations.js
@@ -3,11 +3,13 @@ import flicker from './modes/flicker'
 import randomEcho from './modes/randomEcho'
 import tensionAndRandomEcho from './modes/tensionAndRandomEcho'
 import rainEffect from './modes/rainEffect'
+import basic_test from './modes/basic_test'
 
 export default {
 	basic: basic.basic,
 	flicker: flicker.flicker,
 	randomEcho: randomEcho.randomEcho,
 	tensionAndRandomEcho: tensionAndRandomEcho.tensionAndRandomEcho,
-	rainEffect: rainEffect.rainEffect
+	rainEffect: rainEffect.rainEffect,
+	basic_test: basic_test.basic_test
 }
