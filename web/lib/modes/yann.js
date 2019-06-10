516301b5dd6cc33161adf55fb21fe7876d5e9251
yann marquet <ymarquet@gmail.com>
2019-06-11 00:21:23 +0100

    Best commit message ever - :new_moon_with_face: :cow:

--- web/lib/modes/yann.js
+++ web/lib/modes/yann.js
@@ -1,26 +1,44 @@
 const yann = (sticks, sensors) => {
-  let leds
-  return sensors.map(sensor => {
-    return sticks.map(stick => {
-      const leds = []
-      for (let key = 0; key < stick.numberOfLEDs; key++) {
+  const brightColour = {
+    r: 200,
+    g: 200,
+    b: 255
+  }
+  const offColour = {
+    r: 0,
+    g: 0,
+    b: 0
+  }
 
-        leds.push({
-          number: key,
-          color: {
-            r: 100,
-            g: 100,
-            b: 155
-          }
-        })
+  console.log('yann')
+  return sticks.map(stick => {
+    let leds = Array.from(Array(3), (x, i) => () => {} )
+    const middleLEDIndex = parseInt(stick.numberOfLEDs / 2)
+
+    for (let key = 0; key < stick.numberOfLEDs; key++) {
+
+
+      const movingIndexOfColouredLed = parseInt(sinwave * stick.numberOfLEDs  - parseInt(stick.numberOfLEDs / 2))
+
+
+
+      for(let LedIndex = 0; LedIndex < stick.numberOfLEDs; LedIndex++) {
+        const sinwave = Math.sin(parseInt(stick.name))
+        console.log({stick_name: stick.name, middleLEDIndex: middleLEDIndex, key: key, sinwave: sinwave, movingIndexOfColouredLed: movingIndexOfColouredLed})
+
+        if (LedIndex > Math.abs(movingIndexOfColouredLed) || LedIndex > Math.abs(movingIndexOfColouredLed)) {
+          leds[LedIndex] = { number: key, color: offColour }
+        } else {
+          leds[LedIndex] = { number: key, color: offColour }
+        }
       }
-
-    })
-
-    return {
-      key: stick.name,
-      leds
     }
+    return [
+      {
+        key: stick.name,
+        leds
+      }
+    ]
   })
 }
 
