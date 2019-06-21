a39e7c7af86150f57a4a58378e98a0c637d79130
baitcode <batiyiv@gmail.com>
2019-06-21 20:46:23 +0100

    stolbi

--- web/lib/configuration/arduinosConfig.js
+++ web/lib/configuration/arduinosConfig.js
@@ -1,4 +1,147 @@
 let arduinosConfig = [
+  {
+    "name": "/dev/ttyUSB1",
+    "column": "1",
+    "baudRate": 115200,
+    "numberOfLEDs": 40,
+    "sensors": [
+      {
+        "name": "some",
+        "position": 10
+      }
+    ],
+    "baseTension": 0
+  },
+  {
+    "name": "/dev/ttyUSB2",
+    "column": "2",
+    "baudRate": 115200,
+    "numberOfLEDs": 40,
+    "sensors": [
+      {
+        "name": "some",
+        "position": 10
+      }
+    ],
+    "baseTension": 0
+  },
+  {
+    "name": "/dev/ttyUSB3",
+    "column": "3",
+    "baudRate": 115200,
+    "numberOfLEDs": 40,
+    "sensors": [
+      {
+        "name": "some",
+        "position": 10
+      }
+    ],
+    "baseTension": 0
+  },
+  {
+    "name": "/dev/ttyUSB4",
+    "column": "4",
+    "baudRate": 115200,
+    "numberOfLEDs": 40,
+    "sensors": [
+      {
+        "name": "some",
+        "position": 10
+      }
+    ],
+    "baseTension": 0
+  },
+  {
+    "name": "/dev/ttyUSB5",
+    "column": "5",
+    "baudRate": 115200,
+    "numberOfLEDs": 40,
+    "sensors": [
+      {
+        "name": "some",
+        "position": 10
+      }
+    ],
+    "baseTension": 0
+  },
+  {
+    "name": "/dev/ttyUSB6",
+    "column": "6",
+    "baudRate": 115200,
+    "numberOfLEDs": 40,
+    "sensors": [
+      {
+        "name": "some",
+        "position": 10
+      }
+    ],
+    "baseTension": 0
+  },
+  {
+    "name": "/dev/ttyUSB7",
+    "column": "7",
+    "baudRate": 115200,
+    "numberOfLEDs": 40,
+    "sensors": [
+      {
+        "name": "some",
+        "position": 10
+      }
+    ],
+    "baseTension": 0
+  },
+  {
+    "name": "/dev/ttyUSB8",
+    "column": "8",
+    "baudRate": 115200,
+    "numberOfLEDs": 40,
+    "sensors": [
+      {
+        "name": "some",
+        "position": 10
+      }
+    ],
+    "baseTension": 0
+  },
+  {
+    "name": "/dev/ttyUSB9",
+    "column": "9",
+    "baudRate": 115200,
+    "numberOfLEDs": 40,
+    "sensors": [
+      {
+        "name": "some",
+        "position": 10
+      }
+    ],
+    "baseTension": 0
+  },
+  {
+    "name": "/dev/ttyUSB0",
+    "column": "10",
+    "baudRate": 115200,
+    "numberOfLEDs": 40,
+    "sensors": [
+      {
+        "name": "some",
+        "position": 10
+      }
+    ],
+    "baseTension": 0
+  },
+  {
+    "name": "/dev/ttyAMA0",
+    "column": "11",
+    "baudRate": 115200,
+    "numberOfLEDs": 40,
+    "sensors": [
+      {
+        "name": "some",
+        "position": 10
+      }
+    ],
+    "baseTension": 0
+  }
 ]
 
 function addArduino(arduino) {
