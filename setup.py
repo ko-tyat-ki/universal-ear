f5830f360c1cedc8bd6df520b25cf6d130e043a3
baitcode <batiyiv@gmail.com>
2019-06-21 20:44:26 +0100

    asd

--- setup.py
+++ setup.py
@@ -2,33 +2,49 @@ import requests
 
 ports = [
     {
-        "name": "/dev/ttyAMA0",
-        "column": 1,
-    },
-    {
-        "name": "/dev/ttyUSB0",
-        "column": 2,
+        "name": "/dev/ttyUSB1",
+        "column": '1',
     },
     {
         "name": "/dev/ttyUSB2",
-        "column": 3,
-    },
-    {
-        "name": "/dev/ttyUSB5",
-        "column": 4,
-    },
-    {
-        "name": "/dev/ttyUSB4",
-        "column": 5,
-    },
-    {
-        "name": "/dev/ttyUSB1",
-        "column": 6,
+        "column": '2',
     },
     {
         "name": "/dev/ttyUSB3",
-        "column": 7,
-    }
+        "column": '3',
+    },
+    {
+        "name": "/dev/ttyUSB4",
+        "column": '4',
+    },
+    {
+        "name": "/dev/ttyUSB5",
+        "column": '5',
+    },
+    {
+        "name": "/dev/ttyUSB6",
+        "column": '6',
+    },
+    {
+        "name": "/dev/ttyUSB7",
+        "column": '7',
+    },
+    {
+        "name": "/dev/ttyUSB8",
+        "column": '8',
+    },
+    {
+        "name": "/dev/ttyUSB9",
+        "column": '9',
+    },
+    {
+        "name": "/dev/ttyUSB0",
+        "column": '10',
+    },
+    {
+        "name": "/dev/ttyAMA0",
+        "column": '11',
+    },
 ]
 
 for port in ports:
