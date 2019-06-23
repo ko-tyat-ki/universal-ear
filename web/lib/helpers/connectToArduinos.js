e70bd6f3044e0d75135d270f95ed3be757dc49ca
baitcode <batiyiv@gmail.com>
2019-06-23 19:55:26 +0100

    returned map

--- web/lib/helpers/connectToArduinos.js
+++ web/lib/helpers/connectToArduinos.js
@@ -4,21 +4,7 @@ import { arduinosConfig } from '../configuration/arduinosConfig.js'
 import { RealSensor } from '../classes/realSensor.js'
 
 export const connectToArduinos = () => {
-    let res = []
-
-    for (let arduinoConfig of arduinosConfig) {
-      let sensor
-      try {
-        sensor = new RealSensor(arduinoConfig)
-        console.log(sensor)
-      } catch {
-        console.log('Can\'t create sensor', sensor)
-        continue
-      }
-      if (!sensor) continue
-      res.push(sensor)
-
-    }
-
-    return res
+    return arduinosConfig.map(arduinoConfig => {
+        return new RealSensor(arduinoConfig)
+    }).filter(Boolean)
 }
