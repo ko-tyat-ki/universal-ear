e9e2ba38b01c241f9a593a98db55a565d638e14e
Katerina Loschinina <ekaterina.loshchinina@gmail.com>
2019-06-25 23:12:48 +0100

    Remove comments

--- web/lib/helpers/getDistance.js
+++ web/lib/helpers/getDistance.js
@@ -6,8 +6,5 @@ export const getDistance = ({
     const sensorStick = allSticks.find(stick => {
         return sensor.column === stick.name
     })
-    // console.log('1', sensorStick.init.x - stick.init.x)
-    // console.log('2', sensorStick.init.z - stick.init.z)
-    // console.log('3', (sensorStick.init.x - stick.init.x) ^ 2 + (sensorStick.init.z - stick.init.z) ^ 2)
     return Math.sqrt(Math.pow((sensorStick.init.x - stick.init.x), 2) + Math.pow((sensorStick.init.z - stick.init.z), 2))
 }
