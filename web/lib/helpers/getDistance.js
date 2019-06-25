3ecf3e09a5ec7a368b8d72cf996140f8d5748ad4
Katerina Loschinina <ekaterina.loshchinina@gmail.com>
2019-06-25 23:19:03 +0100

    Fix getDistance
    Previous error was caused by real sensors attempting to connect to
    the client stick that is not there (e.g. more real sensors than client
    sticks)

--- web/lib/helpers/getDistance.js
+++ web/lib/helpers/getDistance.js
@@ -6,5 +6,6 @@ export const getDistance = ({
     const sensorStick = allSticks.find(stick => {
         return sensor.column === stick.name
     })
+    if (!sensorStick) return; // can be undefined if real stick is assigned to a column that is not in the client configure
     return Math.sqrt(Math.pow((sensorStick.init.x - stick.init.x), 2) + Math.pow((sensorStick.init.z - stick.init.z), 2))
 }
