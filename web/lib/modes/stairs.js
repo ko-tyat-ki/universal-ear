d9ed4c4ae2360ded11a4f98cb3e11aa7eb489462
Katerina Loschinina <ekaterina.loshchinina@gmail.com>
2019-06-30 15:22:07 +0100

    Add tesnsion algorythm for 2 sticks

--- web/lib/modes/stairs.js
+++ web/lib/modes/stairs.js
@@ -10,49 +10,73 @@ const superBrightColor = () => {
     }
 }
 
-// const superBrightColor = () => {
-//     return {
-//         r: Math.floor(Math.random() * 1),
-//         g: Math.floor(Math.random() * 20),
-//         b: Math.floor(Math.random() * 20)
-//     }
-// }
-
-const ledsCalculation = (numberOfParts, timeImput, raiseFactor) => {
+const ledsCalculation = ({ numberOfParts, timeImput, raiseFactor, tension }) => {
     return [...Array(NUMBER_OF_LEDS / numberOfParts)].map((el, key) => {
-        return {
-            number: key + NUMBER_OF_LEDS / numberOfParts * ((timeImput + raiseFactor) % numberOfParts),
-            color: superBrightColor()
+        const liftFactor = (timeImput + raiseFactor) % numberOfParts
+        const lift = NUMBER_OF_LEDS / numberOfParts * liftFactor
+        let number
+        let color
+        if (tension > 10) {
+            if (numberOfParts === 2) {
+                if (liftFactor === 0) {
+                    if (key < NUMBER_OF_LEDS / 4) {
+                        number = key + lift
+                    } else {
+                        number = key + lift + NUMBER_OF_LEDS / 4
+                    }
+                } else if (liftFactor === 1) {
+                    if (key < NUMBER_OF_LEDS / 4) {
+                        number = key + lift - NUMBER_OF_LEDS / 4
+                    } else {
+                        number = key + lift
+                    }
+                }
+            }
+            color = {
+                r: 255,
+                g: 0,
+                b: 0
+            }
+        } else {
+            number = key + lift
+            color = superBrightColor()
         }
-    })
+        if (number < 0 || number > NUMBER_OF_LEDS - 1) return
+        return {
+            number,
+            color
+        }
+    }).filter(Boolean)
 }
 
 const speed = 333 // in change per milisecond
 
 const risingStairs = (sticks, sensors) => {
     return sensors.map(sensor => {
-        const tension = sensor.tension;
-
         return sticks.map(stick => {
-            if (sticks.length === 2) {
-                const timeImput = Math.floor((Date.now() - start) / 1000)
-                const raiseFactor = (stick.name === '1') ? 0 : 1
-                return {
-                    key: stick.name,
-                    leds: ledsCalculation(2, timeImput, raiseFactor)
-                };
-            }
-            if (sticks.length === 12) {
-                const timeImput = Math.floor((Date.now() - start) / speed)
-                let raiseFactor
-                if (['3', '7'].indexOf(stick.name) > -1) raiseFactor = 0
-                if (['1', '4', '8', '11'].indexOf(stick.name) > -1) raiseFactor = 1
-                if (['2', '5', '9', '12'].indexOf(stick.name) > -1) raiseFactor = 2
-                if (['6', '10'].indexOf(stick.name) > -1) raiseFactor = 3
-                return {
-                    key: stick.name,
-                    leds: ledsCalculation(4, timeImput, raiseFactor)
-                };
+            if (stick.name === sensor.stick) {
+                const tension = sensor.tension
+                if (sticks.length === 2) {
+                    const timeImput = Math.floor((Date.now() - start) / speed)
+                    const raiseFactor = (stick.name === '1') ? 0 : 1
+                    return {
+                        key: stick.name,
+                        leds: ledsCalculation({ numberOfParts: 2, timeImput, raiseFactor, tension })
+                    };
+                }
+                if (sticks.length === 12) {
+                    const timeImput = Math.floor((Date.now() - start) / speed)
+                    let raiseFactor
+                    if (['3', '7'].indexOf(stick.name) > -1) raiseFactor = 0
+                    if (['1', '4', '8', '11'].indexOf(stick.name) > -1) raiseFactor = 1
+                    if (['2', '5', '9', '12'].indexOf(stick.name) > -1) raiseFactor = 2
+                    if (['6', '10'].indexOf(stick.name) > -1) raiseFactor = 3
+                    return {
+                        key: stick.name,
+                        leds: ledsCalculation({ numberOfParts: 4, timeImput, raiseFactor, tension })
+                    };
+                }
+
             }
         });
     });
