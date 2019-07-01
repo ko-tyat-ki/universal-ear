21b63f425a56eda6380049d186b797cbeff6ed9a
Katerina Loschinina <ekaterina.loshchinina@gmail.com>
2019-07-01 20:45:53 +0100

    Hida rain as well

--- web/lib/visualisations.js
+++ web/lib/visualisations.js
@@ -4,7 +4,6 @@ import random_flashes from './modes/random_flashes'
 import random_flashes_v2 from './modes/random_flashes_v2'
 import flicker from './modes/flicker'
 import tensionWithEcho from './modes/tensionWithEcho'
-import rain from './modes/rain'
 import jasmine from './modes/jasmine'
 import ocean from './modes/ocean'
 import polzynki from './modes/polzynki'
@@ -17,7 +16,6 @@ export default {
 	random_flashes_v2,
 	flicker,
 	tensionWithEcho,
-	rain,
 	jasmine,
 	ocean,
 	polzynki,
