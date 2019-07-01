f39e1e276d6d2b6a972e6ab22a028d3aaa6cde14
Katerina Loschinina <ekaterina.loshchinina@gmail.com>
2019-07-01 20:09:19 +0100

    Remove other not-used modes

--- web/lib/visualisations.js
+++ web/lib/visualisations.js
@@ -3,11 +3,8 @@ import changing_colors from './modes/changing_colors'
 import random_flashes from './modes/random_flashes'
 import random_flashes_v2 from './modes/random_flashes_v2'
 import flicker from './modes/flicker'
-import randomEcho from './modes/randomEcho'
 import tensionWithEcho from './modes/tensionWithEcho'
-import fastReaction from './modes/fastReaction'
 import rain from './modes/rain'
-import yannsIdea from './modes/yannsIdea'
 import jasmine from './modes/jasmine'
 import ocean from './modes/ocean'
 import polzynki from './modes/polzynki'
@@ -19,11 +16,8 @@ export default {
 	random_flashes,
 	random_flashes_v2,
 	flicker,
-	randomEcho,
 	tensionWithEcho,
-	fastReaction,
 	rain,
-	yannsIdea,
 	jasmine,
 	ocean,
 	polzynki,
