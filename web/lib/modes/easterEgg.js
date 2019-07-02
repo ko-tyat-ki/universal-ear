8649c5f7226478ce20a5cb057432de86c1f8fdf4
Katerina Loschinina <ekaterina.loshchinina@gmail.com>
2019-07-02 21:42:52 +0100

    Add easter config

--- web/lib/modes/easterEgg.js
+++ web/lib/modes/easterEgg.js
@@ -1,12 +1,12 @@
 import { simpleRainbow } from "../helpers/rainbowColors";
 import { NUMBER_OF_LEDS } from "../configuration/constants";
+import { easterEggConfig } from '../../../modes_config.json'
 
 let start = Date.now()
-const speed = 150
-const duration = 10 * 1000 // 30 Seconds of fun!
-
-const tensionThreshold = 10
-const activationStickCount = 3
+const speed = easterEggConfig.speed // 150
+const duration = easterEggConfig.speed// 10 seconds, 30 Seconds of fun! - needs to be 2 times melody
+const tensionThreshold = easterEggConfig.speed // 10
+const activationStickCount = easterEggConfig.speed // 3
 
 let modeIsActive = false;
 
@@ -26,7 +26,7 @@ const isTriggered = (sensors, sticks) => {
 }
 
 const isActive = () => {
-    return modeIsActive
+  return modeIsActive
 }
 
 const activate = () => {
@@ -40,41 +40,41 @@ const deactivate = () => {
 }
 
 const canActivate = (sticks, sensors) => {
-    if (!modeIsActive && isTriggered(sensors, sticks)) {
-      activate()
-      return true
-    }
+  if (!modeIsActive && isTriggered(sensors, sticks)) {
+    activate()
+    return true
+  }
 }
 
 // в это время играет музыка бенни хилла
 const easterEgg = (sticks, sensors) => {
-    if (!modeIsActive) return
+  if (!modeIsActive) return
 
-    if (modeIsActive && Date.now() - start > duration) {
-      deactivate()
-      return
+  if (modeIsActive && Date.now() - start > duration) {
+    deactivate()
+    return
+  }
+
+  // Get tension of current sensor
+  // const tension = sensor.tension
+
+  return sticks.map((stick, key) => {
+    const leds = []
+    const numberOfLeds = Math.floor((Date.now() - start) / speed) % NUMBER_OF_LEDS
+
+    {
+      [...Array(numberOfLeds)].map((el, key) => leds.push({
+        number: key,
+        color: simpleRainbow(key)
+      }))
     }
-
-    // Get tension of current sensor
-    // const tension = sensor.tension
-
-    return sticks.map((stick, key) => {
-        const leds = []
-        const numberOfLeds = Math.floor((Date.now() - start) / speed) % NUMBER_OF_LEDS
-
-        {
-            [...Array(numberOfLeds)].map((el, key) => leds.push({
-                number: key,
-                color: simpleRainbow(key)
-            }))
-        }
-        return [
-            {
-                key: stick.name,
-                leds
-            }
-        ]
-    })
+    return [
+      {
+        key: stick.name,
+        leds
+      }
+    ]
+  })
 }
 
 export default {
