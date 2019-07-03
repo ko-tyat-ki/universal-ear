68c38676fa40ee043cd412d8844b0a2a9bf3d81c
Andrey Novikov <Andrey.Novikov@uk.bp.com>
2019-07-04 00:03:52 +0100

    - lite music, mp3

--- sound/run_sound.py
+++ sound/run_sound.py
@@ -30,8 +30,8 @@ while True:
 
 # init mixer
 pygame.mixer.init(44100, -16, channels=2, buffer=4096)
-pygame.mixer.set_num_channels(NUM_CHANNELS + 1)
-channels = [pygame.mixer.Channel(i) for i in range(0, NUM_CHANNELS + 1)]  # argument must be int
+pygame.mixer.set_num_channels(NUM_CHANNELS)
+channels = [pygame.mixer.Channel(i) for i in range(0, NUM_CHANNELS)]  # argument must be int
 
 exit = False
 
@@ -69,21 +69,21 @@ while not exit:
             if mode == "flicker":
                 for sensor in msg['sensorsData']:
                     try:
-                        name = int(sensor['LEDShtuka'])-1
+                        name = int(sensor['LEDShtuka'])
                     except:
                         continue
-                    if name in [1,4,7,8]:
+                    if name in range(1,7):
                         if sensor['fast'] > TRIGGER_LED_VALUE:
-                            if time.time() > channels_ignore[name]:
-                                channels[name].play(sounds[name])
-                                channels_ignore[name] = time.time()+sounds[name].get_length()
-                    if name in [0,2,3,5,6,9,10,11]:
-                        slow = sensor['slow']
-                        if slow < 0:
-                            slow = 0
-                        if slow > MAX_LED_VALUE:
-                            slow = MAX_LED_VALUE
-                        channels[name].set_volume(slow/MAX_LED_VALUE)
+                            if time.time() > channels_ignore[name-1]:
+                                channels[name-1].play(sounds[name-1])
+                                channels_ignore[name-1] = time.time()+sounds[name-1].get_length()
+                    # if name in [0,2,3,5,6,9,10,11]:
+                    #     slow = sensor['slow']
+                    #     if slow < 0:
+                    #         slow = 0
+                    #     if slow > MAX_LED_VALUE:
+                    #         slow = MAX_LED_VALUE
+                    #     channels[name].set_volume(slow/MAX_LED_VALUE)
 
         else: #change of mode
             if cur_mode != "init":
@@ -96,19 +96,15 @@ while not exit:
                     # create separate Channel objects for simultaneous playback
 
 
-                    sounds = [pygame.mixer.Sound(os.path.join(cur_dir, 'flicker',str(x)+'.ogg')) for x in range(0, NUM_CHANNELS)]
-                    sounds.append(pygame.mixer.Sound(os.path.join(cur_dir, 'flicker','Base.ogg')))
+                    sounds = [pygame.mixer.Sound(os.path.join(cur_dir, 'flicker',str(x)+'.ogg')) for x in range(1, 7)]
+
 
                     # play Base sound
+                    pygame.mixer.music.load(os.path.join(cur_dir, 'flicker','Base.mp3'))
+                    pygame.mixer.music.play(loops=-1)
 
-                    channels[12].play(sounds[12], loops=-1)
-
-                    for i in [1,4,7,8]:
+                    for i in range(1,7):
                         channels[i].set_volume(1)
-
-                    for i in [0,2,3,5,6,9,10,11]:
-                        channels[i].set_volume(0)
-                        channels[i].play(sounds[i], loops=-1)
 
                     cur_mode = "flicker"
 
