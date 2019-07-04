600555bfd641f505deba57117f05c1fad50f1152
Katerina Loschinina <ekaterina.loshchinina@gmail.com>
2019-07-05 00:00:45 +0100

    Keep tension positive check

--- web/lib/modes/jasmine.js
+++ web/lib/modes/jasmine.js
@@ -28,42 +28,42 @@ const jasmine = (sticks, sensors) => {
 
         return sticks.map(stick => {
             const leds = []
-            // if (tension >= 0) {
-            const distance = getDistance({
-                sensor,
-                stick,
-                allSticks: sticks
-            })
+            if (tension >= 0) {
+                const distance = getDistance({
+                    sensor,
+                    stick,
+                    allSticks: sticks
+                })
 
-            if (distance === 0) {
-                if (!sensor.isSlowSensor && sensor.stick === stick.name && tension >= jasmineConfig.tensionTrigger) {
-                    [...Array(NUMBER_OF_LEDS)].map((el, key) => leds.push({
-                        number: key,
-                        color: communistColor
-                    }))
-                }
-            } else {
-                if (tension) {
-                    {
-                        [...Array(NUMBER_OF_LEDS)].map((el, key) => {
-                            const randomWarmColor = {
-                                r: randomGenerator[count][key].r,
-                                g: randomGenerator[count][key].g,
-                                b: randomGenerator[count][key].b
-                            }
-                            count++
-                            if (count === numberOfRandomness - 1) count = 0
-                            if (randomWarmColor.r + randomWarmColor.g + randomWarmColor.b > 450) {
-                                leds.push({
-                                    number: key,
-                                    color: randomWarmColor
-                                })
-                            }
-                        })
+                if (distance === 0) {
+                    if (!sensor.isSlowSensor && sensor.stick === stick.name && tension >= jasmineConfig.tensionTrigger) {
+                        [...Array(NUMBER_OF_LEDS)].map((el, key) => leds.push({
+                            number: key,
+                            color: communistColor
+                        }))
+                    }
+                } else {
+                    if (tension) {
+                        {
+                            [...Array(NUMBER_OF_LEDS)].map((el, key) => {
+                                const randomWarmColor = {
+                                    r: randomGenerator[count][key].r,
+                                    g: randomGenerator[count][key].g,
+                                    b: randomGenerator[count][key].b
+                                }
+                                count++
+                                if (count === numberOfRandomness - 1) count = 0
+                                if (randomWarmColor.r + randomWarmColor.g + randomWarmColor.b > 450) {
+                                    leds.push({
+                                        number: key,
+                                        color: randomWarmColor
+                                    })
+                                }
+                            })
+                        }
                     }
                 }
             }
-            // }
 
             return {
                 key: stick.name,
