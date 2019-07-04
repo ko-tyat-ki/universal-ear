6d74746b7be5415f36f6cc31c423650ef7ef3b1c
Katerina Loschinina <ekaterina.loshchinina@gmail.com>
2019-07-04 15:47:11 +0100

    Improve jasmine

--- web/lib/modes/jasmine.js
+++ web/lib/modes/jasmine.js
@@ -12,46 +12,44 @@ const jasmine = (sticks, sensors) => {
     return sensors.map(sensor => {
         const tension = sensor.tension
 
-        if (tension) {
-            return sticks.map(stick => {
-                const leds = []
-                if (sensor && tension >= 0) {
-                    const distance = getDistance({
-                        sensor,
-                        stick,
-                        allSticks: sticks
-                    })
+        return sticks.map(stick => {
+            const leds = []
+            if (sensor && tension >= 0) {
+                const distance = getDistance({
+                    sensor,
+                    stick,
+                    allSticks: sticks
+                })
 
-                    if (distance === 0) {
-                        if (!sensor.isSlowSensor && sensor.stick === stick.name && tension >= jasmineConfig.tensionTrigger) {
-                            [...Array(NUMBER_OF_LEDS)].map((el, key) => leds.push({
-                                number: key,
-                                color: communistColor
-                            }))
-                        }
-                    } else {
-                        [...Array(NUMBER_OF_LEDS)].map((el, key) => {
-                            const randomWarmColor = {
-                                r: Math.floor(Math.random() * 255),
-                                g: Math.floor(Math.random() * 150),
-                                b: Math.floor(Math.random() * 100)
-                            }
-
-                            if (randomWarmColor.r + randomWarmColor.g + randomWarmColor.b > 450)
-                                leds.push({
-                                    number: key,
-                                    color: randomWarmColor
-                                })
-                        })
+                if (distance === 0) {
+                    if (!sensor.isSlowSensor && sensor.stick === stick.name && tension >= jasmineConfig.tensionTrigger) {
+                        [...Array(NUMBER_OF_LEDS)].map((el, key) => leds.push({
+                            number: key,
+                            color: communistColor
+                        }))
                     }
-                }
+                } else {
+                    [...Array(NUMBER_OF_LEDS)].map((el, key) => {
+                        const randomWarmColor = {
+                            r: Math.floor(Math.random() * 255),
+                            g: Math.floor(Math.random() * 150),
+                            b: Math.floor(Math.random() * 100)
+                        }
 
-                return {
-                    key: stick.name,
-                    leds: leds.filter(Boolean)
+                        if (randomWarmColor.r + randomWarmColor.g + randomWarmColor.b > 450 && tension)
+                            leds.push({
+                                number: key,
+                                color: randomWarmColor
+                            })
+                    })
                 }
-            })
-        }
+            }
+
+            return {
+                key: stick.name,
+                leds: leds.filter(Boolean)
+            }
+        })
     })
 }
 
