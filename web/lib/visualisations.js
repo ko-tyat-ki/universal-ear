73cd47ffe3e860cebb43117ef698493221c368cf
baitcode <batiyiv@gmail.com>
2019-07-05 01:01:05 +0100

    Sleep mode is changed to random flashes v2

--- web/lib/visualisations.js
+++ web/lib/visualisations.js
@@ -1,7 +1,6 @@
 import basic_with_rainbow from './modes/basic_with_rainbow'
 import changing_colors from './modes/changing_colors'
 import random_flashes from './modes/random_flashes'
-import random_flashes_v2 from './modes/random_flashes_v2'
 import flicker from './modes/flicker'
 import tensionWithEcho from './modes/tensionWithEcho'
 import jasmine from './modes/jasmine'
@@ -13,7 +12,6 @@ export default {
 	basic_with_rainbow,
 	changing_colors,
 	random_flashes,
-	random_flashes_v2,
 	flicker,
 	tensionWithEcho,
 	jasmine,
