b7df2471f879b75a0b9214784f05f80dd35cde6e
Andrey Novikov <Andrey.Novikov@uk.bp.com>
2019-07-05 15:20:32 +0100

    - proper logging

--- sound/run_sound.py
+++ sound/run_sound.py
@@ -5,12 +5,17 @@ import datetime as dt
 import random
 import socket
 import os
+import logging
+
+logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
 
 FADEOUT_TIME = 5
 NUM_CHANNELS = 12
 MAX_LED_VALUE = 40
 TRIGGER_LED_VALUE = 30
 t = time.time()
+
+logging.info("Initialisation for {} channels".format(NUM_CHANNELS))
 
 channels_ignore = [0 for i in range(0, NUM_CHANNELS)]
 sounds = []
@@ -19,6 +24,8 @@ cur_mode = "init"
 
 cur_dir = (os.path.dirname(os.path.abspath(__file__)))
 
+logging.info("Trying to connect...")
+
 # set up and open read from the socket
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 while True:
@@ -26,8 +33,10 @@ while True:
         client.bind(('localhost', 8124))
         break
     except:
-        print("Can't bind to 8124. Retry")
+        logging.warning("Can't bind to 8124. Retry")
         time.sleep(1)
+
+logging.info("Initialising mixer")
 
 # init mixer
 pygame.mixer.init(44100, -16, channels=2, buffer=4096)
@@ -38,10 +47,10 @@ exit = False
 
 while not exit:
     try:
-        print('Listening for new connection...')
+        logging.info('Listening for new connection...')
         client.listen(1)
         c, addr = client.accept()
-        print('Got connection from ', addr)
+        logging.info('Got connection from {}'.format(addr))
 
         while True: # infinite while-loop
             json_msg = c.recv(4096) # receives sensor value from the socket
@@ -53,12 +62,13 @@ while not exit:
             if msg_point != -1:
                 json_msg = json_msg[:msg_point+1]
 
-            #print("{}:{}".format(dt.datetime.now().time(),json_msg))
+            logging.debug("{}:{}".format(dt.datetime.now().time(),json_msg))
 
             msg = json.loads(json_msg)
 
             if fadeout_ignore:
                 if time.time()-t > FADEOUT_TIME:
+                    logging.info('Fadeout complete')
                     fadeout_ignore = False
                     cur_mode = "init"
                     channels_ignore = [False for i in range(0,NUM_CHANNELS)] #init channel ignores
@@ -100,7 +110,9 @@ while not exit:
 
 
             else: #change of mode
+                logging.info("Change of mode from {} to {}".format(cur_mode,mode))
                 if mode == "easterEgg":
+                    logging.info("Easter Egg!")
                     # UNCOMMENT FOR REAL EASTER EGG :)
                     # pygame.mixer.stop()
 
@@ -112,14 +124,18 @@ while not exit:
                     pygame.mixer.music.play(loops=1)
                     cur_mode = mode
                 elif cur_mode != "init":
+                    logging.info("Start Fadeout")
                     t = time.time()
                     pygame.mixer.fadeout(FADEOUT_TIME * 1000)
                     pygame.mixer.music.fadeout(FADEOUT_TIME*1000)
                     fadeout_ignore = True
                  #init mode sounds
                 else:
+                    logging.info("Load new mode {}".format(mode))
                     if mode in ["flicker","jasmine"]:
                         # create separate Channel objects for simultaneous playback
+
+                        logging.info("Loading {}".format(mode))
 
                         sounds = []
 
@@ -139,7 +155,9 @@ while not exit:
                         pygame.mixer.music.play(loops=-1)
 
                         cur_mode = mode
-                    if mode in ["sleep"]:
+                    elif mode in ["sleep"]:
+                        logging.info("Loading {}".format(mode))
+
                         # play Base sound
                         try:
                             pygame.mixer.music.load(os.path.join(cur_dir, mode,'Base.mp3'))
@@ -148,8 +166,15 @@ while not exit:
                         pygame.mixer.music.set_volume(0.8)
                         pygame.mixer.music.play(loops=-1)
 
+                        cur_mode = mode
+                    else:
+                        logging.info("Default catch - empty mode")
+                        cur_mode = mode
+
+                    logging.info("Finished loading {}".format(mode))
+
     except Exception as e:
-        print("Exception: {}".format(e))
+        logging.exception("Exception: {}".format(e))
         pass
 
 
