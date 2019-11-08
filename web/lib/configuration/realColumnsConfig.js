8bfe819a307bbbe39bb0824af6544a56f6fa2b78
Katerina Loschinina <ekaterina.loshchinina@gmail.com>
2019-11-08 10:31:32 +0000

    Rename realSticksConfig into realColumnsConfig

---
+++ web/lib/configuration/realColumnsConfig.js
@@ -0,0 +1,275 @@
+/* global console */
+
+export const calculateRealColumns = (selectedStructure) => {
+    switch (selectedStructure) {
+        case 'duet':
+            return [{
+                numberOfLEDs: 40,
+                name: '1',
+                init: {
+                    x: 125,
+                    y: -180,
+                    z: 0
+                }
+            },
+            {
+                numberOfLEDs: 40,
+                name: '2',
+                init: {
+                    x: -125,
+                    y: -180,
+                    z: 0
+                }
+            }]
+        case 'circle':
+            return [{
+                numberOfLEDs: 40,
+                name: '1',
+                init: {
+                    x: 597,
+                    y: -180,
+                    z: 0
+                }
+            },
+            {
+                numberOfLEDs: 40,
+                name: '2',
+                init: {
+                    x: 597 * Math.cos(2 * Math.PI * 1 / 11),
+                    y: -180,
+                    z: 597 * Math.sin(2 * Math.PI * 1 / 11)
+                }
+            },
+            {
+                numberOfLEDs: 40,
+                name: '3',
+                init: {
+                    x: 597 * Math.cos(2 * Math.PI * 2 / 11),
+                    y: -180,
+                    z: 597 * Math.sin(2 * Math.PI * 2 / 11)
+                }
+            },
+            {
+                name: '4',
+                init: {
+                    x: 597 * Math.cos(2 * Math.PI * 3 / 11),
+                    y: -180,
+                    z: 597 * Math.sin(2 * Math.PI * 3 / 11)
+                }
+            },
+            {
+                numberOfLEDs: 40,
+                name: '5',
+                init: {
+                    x: 597 * Math.cos(2 * Math.PI * 4 / 11),
+                    y: -180,
+                    z: 597 * Math.sin(2 * Math.PI * 4 / 11)
+                }
+            },
+            {
+                name: '6',
+                init: {
+                    x: 597 * Math.cos(2 * Math.PI * 5 / 11),
+                    y: -180,
+                    z: 597 * Math.sin(2 * Math.PI * 5 / 11)
+                }
+            },
+            {
+                numberOfLEDs: 40,
+                name: '7',
+                init: {
+                    x: 597 * Math.cos(2 * Math.PI * 6 / 11),
+                    y: -180,
+                    z: 597 * Math.sin(2 * Math.PI * 6 / 11)
+                }
+            },
+            {
+                numberOfLEDs: 40,
+                name: '8',
+                init: {
+                    x: 597 * Math.cos(2 * Math.PI * 7 / 11),
+                    y: -180,
+                    z: 597 * Math.sin(2 * Math.PI * 7 / 11)
+                }
+            },
+            {
+                numberOfLEDs: 40,
+                name: '9',
+                init: {
+                    x: 597 * Math.cos(2 * Math.PI * 8 / 11),
+                    y: -180,
+                    z: 597 * Math.sin(2 * Math.PI * 8 / 11)
+                }
+            },
+            {
+                numberOfLEDs: 40,
+                name: '10',
+                init: {
+                    x: 597 * Math.cos(2 * Math.PI * 9 / 11),
+                    y: -180,
+                    z: 597 * Math.sin(2 * Math.PI * 9 / 11)
+                }
+            },
+            {
+                numberOfLEDs: 40,
+                name: '11',
+                init: {
+                    x: 597 * Math.cos(2 * Math.PI * 10 / 11),
+                    y: -180,
+                    z: 597 * Math.sin(2 * Math.PI * 10 / 11)
+                }
+            }]
+        case 'nowhere2019':
+            return [
+                {
+                    numberOfLEDs: 40,
+                    name: '1',
+                    init: {
+                        x: 125,
+                        y: -180,
+                        z: 0
+                    }
+                },
+                {
+                    numberOfLEDs: 40,
+                    name: '2',
+                    init: {
+                        x: -125,
+                        y: -180,
+                        z: 0
+                    }
+                },
+                {
+                    numberOfLEDs: 40,
+                    name: '3',
+                    init: {
+                        x: 0,
+                        y: -180,
+                        z: 0
+                    }
+                },
+                {
+                    numberOfLEDs: 40,
+                    name: '4',
+                    init: {
+                        x: 125,
+                        y: -180,
+                        z: -250
+                    }
+                },
+                {
+                    numberOfLEDs: 40,
+                    name: '5',
+                    init: {
+                        x: 75,
+                        y: -140,
+                        z: 75
+                    }
+                },
+                {
+                    numberOfLEDs: 40,
+                    name: '6',
+                    init: {
+                        x: 225,
+                        y: -140,
+                        z: 75
+                    }
+                },
+                {
+                    numberOfLEDs: 40,
+                    name: '7',
+                    init: {
+                        x: -225,
+                        y: -140,
+                        z: -75
+                    }
+                },
+                {
+                    numberOfLEDs: 40,
+                    name: '8',
+                    init: {
+                        x: -75,
+                        y: -140,
+                        z: -75
+                    }
+                },
+                {
+                    numberOfLEDs: 40,
+                    name: '9',
+                    init: {
+                        x: 75,
+                        y: -140,
+                        z: -75
+                    }
+                },
+                {
+                    numberOfLEDs: 40,
+                    name: '10',
+                    init: {
+                        x: 225,
+                        y: -140,
+                        z: -75
+                    }
+                },
+                {
+                    numberOfLEDs: 40,
+                    name: '11',
+                    init: {
+                        x: -75,
+                        y: -140,
+                        z: -225
+                    }
+                },
+                {
+                    numberOfLEDs: 40,
+                    name: '12',
+                    init: {
+                        x: 75,
+                        y: -140,
+                        z: -225
+                    }
+                }]
+        case 'summerRave2019':
+            return [
+                {
+                    numberOfLEDs: 40,
+                    name: '1',
+                    init: {
+                        x: 125,
+                        y: -180,
+                        z: 0
+                    }
+                },
+                {
+                    numberOfLEDs: 40,
+                    name: '2',
+                    init: {
+                        x: -125,
+                        y: -180,
+                        z: 0
+                    }
+                },
+                {
+                    numberOfLEDs: 40,
+                    name: '3',
+                    init: {
+                        x: 0,
+                        y: -180,
+                        z: 0
+                    }
+                },
+                {
+                    numberOfLEDs: 40,
+                    name: '4',
+                    init: {
+                        x: 125,
+                        y: -180,
+                        z: -250
+                    }
+                }]
+        default:
+            console.log('You need to pick up structure you would use')
+            return
+    }
+}
+
